792673f70505b08718c71c31ec2b49c1cde3c5a0
germanmidweekleague <germanmidweekleague@gmail.com>
2018-05-24 07:08:52 +0200

    adding a whole lot of design elements like transitionings, colordesigns and bootstrapresponsitivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {BlogComponent} from './blog/blog.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  mobileQuery: MediaQueryList;
+
+  fillerNav = ["blog",
+               "season 4"];
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,8 @@
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+import 'web-animations-js';import 'web-animations-js/web-animations.min';
+import 'hammerjs';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
